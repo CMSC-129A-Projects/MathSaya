5c724360c8b3cd9f3de8bd62f2ef52905c760397
JerryMae <jggabutan@up.edu.ph>
2021-05-08 21:46:36 +0800

    View Lesson UI

---
+++ ViewLessons/screens/lessons.js
@@ -0,0 +1,21 @@
+const lessonContent = [
+    {
+        time: '5:35 mins',
+        title: "Welcome to the Course",
+    },
+    {time: '1:30 mins', title: 'Addition'},
+    {time: '1:30 mins', title: 'Subtraction'},
+    {time: '2:00 mins', title: 'Multiplication'},
+    {time: '2:30 mins', title: 'Division'},
+    {time: '3:00 mins', title: 'Division'},
+    ];
+    
+    const lessons = [
+    {
+        name: 'Operations',
+        totalLesson: '4',
+        image: require('../assets/math4.jpg'),
+        lessonContent,
+    },];
+    
+    export default lessons;
