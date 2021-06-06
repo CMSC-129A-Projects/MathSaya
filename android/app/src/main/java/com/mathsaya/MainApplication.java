6996995d8de0a2eac1cb98460682897a7b814ef3
JerryMae <jggabutan@up.edu.ph>
2021-06-06 23:35:35 +0800

    Updated HomeScreen and fixed some errors

--- android/app/src/main/java/com/mathsaya/MainApplication.java
+++ android/app/src/main/java/com/mathsaya/MainApplication.java
@@ -32,8 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
           //packages.add(new MyReactNativePackage());
           //packages.add(new SQLitePluginPackage());
           //packages.add(new MainReactPackage(),
-            new RNGestureHandlerPackage(),
-            new RNGestureHandlerPackage());
+            new RNGestureHandlerPackage();
+            new RNGestureHandlerPackage();
           new SQLitePluginPackage();
           return packages;
         }
--- screens/Criteriascreen.js
+++ screens/Criteriascreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Component }  from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity, FlatList, StyleSheet} from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity, FlatList, StyleSheet, ImageBackground} from 'react-native';
 import Grades from './Grades';
 import {windowHeight, windowWidth} from '../utils/Dimensions';
 // import SQLite from 'react-native-sqlite-storage';
@@ -40,34 +40,48 @@ const Criteriascreen = ({ navigation }) => {
             <View 
                 // key = {item.grade_id}
                 style={{
-                    paddingVertical: 10, 
+                    marginTop:20,
+                    paddingVertical: 6,
+                    paddingHorizontal: 9, 
                     flexDirection: 'row',
                     alignItems: 'center',
                     alignContent: 'center',
                 }}>
                 <TouchableOpacity activeOpacity={0.8} onPress={()=>navigation.navigate( "Lessons", {data:grade})}>
                     <View style ={{
-                            backgroundColor: '#dfbf9f',
+                            backgroundColor: '#fefac0',
                             borderTopRightRadius: 10,
                             borderTopLeftRadius: 10,
                             borderBottomLeftRadius: 10,
                             borderBottomRightRadius: 10,
                             paddingHorizontal: 20, 
                             paddingVertical: 10, 
-                            width:windowWidth / 1.112,
+                            width:windowWidth / 1.04,
                             height:windowHeight / 7,
-                            alignItems: 'center',
                             alignContent: 'center',
                         }}>
                         <Text style = {{
-                                fontSize: 59, 
-                                color: '#604020', 
+                                fontSize: 50, 
+                                color: '#402A03', 
                                 paddingTop: 20,
-                                alignItems: 'center', 
-                                textAlign: 'center',
+                                alignItems: 'baseline', 
+                                textAlign: 'left',
                                 fontFamily: 'Foundation'
                             }}>{grade.name}
                         </Text>
+                        <ImageBackground source={grade.image} 
+                            style =
+                            {{
+                                width: windowWidth / 2.42, 
+                                height: windowHeight / 8,
+                                overflow:'hidden',
+                                marginLeft: 205,
+                                marginTop: -63,
+                                marginBottom: -65,
+                                borderBottomLeftRadius:10,
+                                borderTopLeftRadius: 10,
+                            }}>
+                        </ImageBackground>
                     </View>
                 </TouchableOpacity>
             </View>
@@ -79,22 +93,34 @@ const Criteriascreen = ({ navigation }) => {
             style={{
                 backgroundColor: '#fefac0', 
                 flex: 1, 
-                paddingHorizontal: 20,
             }}>
-            <View style={{marginTop: 15}}>
-                <Text style={{fontSize:60, fontWeight: 'bold', color: '#4d2600', fontFamily:'Feather'}}>
+            <View style={{marginTop: 5, paddingHorizontal:10}}>
+                <Text style={{fontSize:65, fontWeight: 'bold', color: '#05375a', fontFamily:'Feather'}}>
                     Kumusta,
                 </Text>
-                <Text style={{fontSize:25, color: '#663300', fontStyle: 'italic'}}>
+                <ImageBackground source={require('../assets/image4.png')} 
+                    style = {{
+                        height: windowHeight / 4.5,
+                        width: windowWidth/3, 
+                        marginLeft: 250,
+                        marginTop: -60,
+                    }}>
+                </ImageBackground>
+                <Text style={{fontSize:25, color: '#05375a', marginTop:-125}}>
                     Tara ug magkat - on kita!
                 </Text>
-                <View style={{marginTop: 5}}>
-                    <Text style={{fontSize:40, fontWeight:'bold', color: '#4d2600'}}>
-                        Mga Kategoriya
-                    </Text>
-                </View>
+                <Text style={{fontSize:40, color: '#05375a', marginTop:60}}>
+                    Mga Kategoriya
+                </Text>
             </View> 
-            <View style={{flex: 1}}>
+            <View style=
+                {{
+                    marginTop: 10,
+                    flex: 1,
+                    backgroundColor: '#F9E671',
+                    borderTopRightRadius: 25,
+                    borderTopLeftRadius: 25,
+                }}>
                 <FlatList
                     showsVerticalScrollIndicator={false} 
                     data={Grades}
